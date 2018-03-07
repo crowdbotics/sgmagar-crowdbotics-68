6499c785dbdfc8a015c8440d8c198dfbaf99bf62
Saurav <sp.gharti@gmail.com>
2018-03-07 22:12:04 +0000

    add package aa_stripe

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
     ]
     context = {
         'title': 'sgmagar-crowdbotics-68',
