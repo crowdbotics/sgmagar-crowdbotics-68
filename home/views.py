62831585e92e6ef1109482641216538127404649
Saurav <sp.gharti@gmail.com>
2018-03-07 22:12:09 +0000

    add package django-paypal

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-paypal', 'url': 'http://pypi.python.org/pypi/django-paypal/0.4.1'},
 	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
     ]
     context = {
