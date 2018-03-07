8ac1efb1b668d339e9c1c86576990f82fad08980
Saurav <sp.gharti@gmail.com>
2018-03-07 22:12:16 +0000

    add package dj-stripe

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'dj-stripe', 'url': 'http://pypi.python.org/pypi/dj-stripe/1.0.0.post1'},
 	{'name':'django-paypal', 'url': 'http://pypi.python.org/pypi/django-paypal/0.4.1'},
 	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
     ]
